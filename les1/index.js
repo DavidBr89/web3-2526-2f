a041ddbf7d3c3420bda5183ee22997c014f34787
DavidBr89 <david.breckx@telenet.be>
2026-02-18 12:18:31 +0100

    Einde les 2

--- les1/index.js
+++ les1/index.js
@@ -125,3 +125,150 @@ const {
 console.log(voornaam);
 console.log(address);
 console.log(city);
+
+// Simuleren pizza restaurant
+
+// Stap 1: Deeg uitrollen
+// setTimeout(() => {
+//   console.log("Deeg uitgerold");
+//   // Stap 2: Toppings erop
+//   setTimeout(() => {
+//     console.log("Toppings liggen erop");
+//     // Stap 3: Pizza in de oven
+//     setTimeout(() => {
+//       console.log("Pizza is gebakken");
+//     }, 500);
+//   }, 2000);
+// }, 1000);
+
+// Promises
+
+// Producing code -> meestal gaan we dit zelf niet schrijven
+
+const myPromise = new Promise((resolve, reject) => {
+  // API call
+  // fetch("https://google.com")
+
+  // GET request vertrokken
+  setTimeout(() => {
+    // Resultaat van de server ontvangen
+    const isSuccess = false;
+    if (isSuccess) {
+      const data = [{ id: 1, name: "Web 3" }];
+
+      resolve(data);
+    } else {
+      reject({ code: 500, msg: "Fout!" });
+    }
+  }, 1000);
+});
+
+myPromise
+  .then((data) => {
+    console.log("Promise fulfilled: ", data);
+  })
+  .catch((err) => {
+    console.log("Promise rejected: ", err);
+  });
+
+// Uitleg voor het chainen van methodes
+const naivePromise = {
+  then: function () {
+    console.log("THEN");
+    return this;
+  },
+  catch: function () {
+    console.log("CATCH");
+    return this;
+  },
+  finally: function () {
+    console.log("FINALLY");
+  },
+};
+
+naivePromise.then().catch().finally();
+
+// Pizza voorbeeld met Promises
+
+const doughPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Deeg is uitgerold");
+  }, 1000);
+});
+
+const toppingsPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Toppings liggen erop");
+  }, 2000);
+});
+
+const ovenPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const isOvenBroken = true;
+    if (!isOvenBroken) {
+      resolve("Pizza is gebakken");
+    } else {
+      reject("Oven is kapot");
+    }
+  }, 500);
+});
+
+// ovenPromise.catch((err) => console.log(err));
+
+// doughPromise
+//   .then((msg) => {
+//     console.log(msg);
+//     return toppingsPromise;
+//   })
+//   .then((toppingsMsg) => {
+//     console.log(toppingsMsg);
+//     return ovenPromise;
+//   })
+//   .then((ovenMsg) => {
+//     console.log(ovenMsg);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Promise.all
+
+Promise.all([doughPromise, ovenPromise, toppingsPromise])
+  .then((arrOfData) => {
+    console.log(arrOfData);
+  })
+  .catch((arrOfErrors) => {
+    console.log(arrOfErrors);
+  });
+
+// ASYNC / AWAIT
+
+const handlePizzaResto = async () => {
+  try {
+    const doughData = await doughPromise;
+    console.log("ASYNC/AWAIT", doughData);
+    const toppingsData = await toppingsPromise;
+    console.log("ASYNC/AWAIT", toppingsData);
+    const ovenData = await ovenPromise;
+    console.log("ASYNC/AWAIT", ovenData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+handlePizzaResto();
+
+const sumFn = () => {};
+
+const anotherFunc = (a, cb) => {
+  cb(a);
+};
+
+anotherFunc(5, () => {});
+
+const anotherAnotherFunc = () => {
+  return () => {};
+};
+
+const returnOfAnotherAnotherFunc = anotherAnotherFunc();
+returnOfAnotherAnotherFunc();
